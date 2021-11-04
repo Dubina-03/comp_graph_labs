b918526a9a9072d8ebdb7bd1f4527db9f2bd0de7
Dubina Olya <olyadubina1623@gmail.com>
2021-11-05 00:22:25 +0200

    lab2: program

---
+++ lab2.py
@@ -0,0 +1,11 @@
+from os import name, path
+import pandas as pd
+from graphics import *
+from PIL import Image, ImageDraw
+
+data=pd.read_csv("DS9.txt", sep=" ", names=["x", "y"])
+image = Image.new("RGB", (540, 960), "white")
+draw = ImageDraw.Draw(image)
+for i in range(len(data)):
+    draw.text((data.loc[i,"x"], data.loc[i,"y"]), text=".", fill="black")
+image.save("lab2.png")
